1ece37e47e5baaf59c2e5b0b0c970d68dc44bf19
kevin <466007639@qq.com>
2018-12-01 21:58:29 +0800

    mybatis generater

---
+++ src/main/java/com/kevin/generatdb/GeneratorExecution.java
@@ -0,0 +1,38 @@
+package com.kevin.generatdb;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ttms
+ * Date: 2018/6/4
+ */
+public class GeneratorExecution {
+    public void generator() {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        try {
+            File configFile = new File(this.getClass().getClassLoader().getResource("generatorConfig.xml").getPath());
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(configFile);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new GeneratorExecution().generator();
+    }
+
+}
---
+++ src/test/java/com/kevin/generatdb/GeneratdbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kevin.generatdb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeneratdbApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
